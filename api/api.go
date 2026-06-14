11552b7ae2a022e0dc0dbabcc0067bfbac99a7ac
erickkempner <erichcardosokempner@gmail.com>
2026-06-14 14:53:20 -0300

    edit and delete routes

--- api/api.go
+++ api/api.go
@@ -31,6 +31,8 @@ func NewHandler(db DB) http.Handler {
 	r.Use(middleware.Logger)
 
 	r.Post("/api/short", handlePost(db))
+	r.Delete("/api/short/{code}", handleDelete(db))
+	r.Put("/api/short/{code}", handleEdit(db))
 	r.Get("/{code}", handleGet(db))
 
 	return r
@@ -63,9 +65,49 @@ func handlePost(db DB) http.HandlerFunc {
 func handleGet(db DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		codeStr := chi.URLParam(r, "code")
-		if redirectURL, ok := db[codeStr]; ok {
-			http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+		redirectURL, ok := db[codeStr]
+		if !ok {
+			SendJSON(w, Response{Error: "code not found"}, http.StatusNotFound)
+			return
 		}
+		http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+	}
+}
+
+func handleEdit(db DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		codeStr := chi.URLParam(r, "code")
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			SendJSON(w, Response{Error: "invalid body"}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		if _, err := url.Parse(body.URL); err != nil {
+			SendJSON(w, Response{Error: "invalid url passed"}, http.StatusBadRequest)
+		}
+
+		if !strings.HasPrefix(body.URL, "https") {
+			SendJSON(w, Response{Error: "only https urls are allowed"}, http.StatusBadRequest)
+			return
+		}
+
+		db[codeStr] = body.URL
+
+		SendJSON(w, Response{Data: "Successfully Edited"}, http.StatusCreated)
+
+	}
+}
+
+func handleDelete(db DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		codeStr := chi.URLParam(r, "code")
+		if _, ok := db[codeStr]; !ok {
+			SendJSON(w, Response{Error: "code not found"}, http.StatusNotFound)
+			return
+		}
+		delete(db, codeStr)
+		SendJSON(w, Response{Data: "Successfully Deleted"}, http.StatusCreated)
 
 	}
 }
